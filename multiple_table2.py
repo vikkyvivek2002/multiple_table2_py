9349316327fe8ceda69783fbccd7daddb348d1cc
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-11-05 20:28:43 +0530

    Add files via upload

---
+++ multiple_table2.py
@@ -0,0 +1,10 @@
+n=int(input('enter a number'))
+r=int(input('entr starting point'))
+e=int(input('enter ending point'))
+for i in range(r,e+1):
+    print(n,'*', i ,' = ', n*i)
+for i in range(r,e-1,-1):
+    print(n,'*', i ,' = ', n*i)
+"""this multiline comments
+this is vivek ..
+"""
